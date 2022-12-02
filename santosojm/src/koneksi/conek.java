8d9defde6d74db08bd7caa0949211f7cef7b0af8
angga0310 <90076969+angga0310@users.noreply.github.com>
2022-12-02 10:48:13 +0700

    Add files via upload

---
+++ santosojm/src/koneksi/conek.java
@@ -0,0 +1,19 @@
+package koneksi;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conek {
+    private static Connection koneksi;
+    
+    public static Connection GetConnection () throws SQLException{
+            if (koneksi == null){
+                new Driver();
+                
+            koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_akhir", "root","");    
+            }
+            return koneksi;
+    }
+}
